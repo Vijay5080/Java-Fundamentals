53d91762faacca0e626ac537fb76208ded2e3fc6
Vijay Badi <98895441+Vijay5080@users.noreply.github.com>
2024-01-31 22:56:55 +0530

    first commit

---
+++ Day1/SecondProgram.java
@@ -0,0 +1,18 @@
+package com.tnsif.day1;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+
+
+	double firstNumber =20d;
+		double secondNumber = 30d;
+		
+		double result;
+		result = firstNumber  / secondNumber;
+		System.out.println("result is " +result);
+		
+
+	}
+
+}
